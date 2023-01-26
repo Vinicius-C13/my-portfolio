fc9fbb9aa0218ae8a0d3f7fba7f8ffda26036c0c
Vinícius Costa <96350207+Vinicius-C13@users.noreply.github.com>
2023-01-25 22:17:40 -0300

    Finished first section.

--- src/components/FirstSection.js
+++ src/components/FirstSection.js
@@ -8,30 +8,36 @@ import ellipse from '../assets/ellipse.svg';
 export default function Section() {
 
     return (
-        <div id='first-section'>
-            <header>
-                <span>Vinícius Costa</span>
-                <div className='photo-container'>
-                    <ul className='icons-container'>
-                        <li><img src={linkedinIcon} alt='linkedin'/></li>
-                        <li><img src={githubIcon} alt='github'/></li>
-                    </ul>
-                    <div>
-                        <img className='photo' src={require('../assets/Foto-pb.png')}/>
+        <>
+            <div class="section-container">
+                <header>
+                    <span class='logo'>vinícius costa</span>
+                    <div className='photo-container'>
+                        <ul className='icons-container'>
+                            <li><img src={linkedinIcon} alt='linkedin'/></li>
+                            <li><img src={githubIcon} alt='github'/></li>
+                        </ul>
+                        <div>
+                            <img className='ellipse' src={ellipse} />
+                            <img className='photo' src={require('../assets/Foto-pb.png')}/>
+                        </div>
                     </div>
-                </div>
+                </header>
                 <img src={navButton} alt='nav button' />
-            </header>
-            <div className='text-container'>
-                <h1>
-                    Prazer em te conhecer!<br/>
-                    Sou o Vinícius Costa.
-                </h1>
-                <p>
-                    De SBC - SP, sou um desenvolvedor web front-end apaixonado por construir 
-                    aplicações bonitas e responsivas.
-                </p>
             </div>
-        </div>
+            <div class="section-container">
+                <img src={rings} id='rings' />
+                <div id='first-section'>
+                    <h1>
+                        Prazer em te conhecer!<br/>
+                        Sou o <span>Vinícius Costa</span>.
+                    </h1>
+                    <p>
+                        De SBC - SP, sou um desenvolvedor web front-end apaixonado por construir
+                        aplicações bonitas e responsivas.
+                    </p>
+                </div>
+            </div>
+        </>
     )
 }
